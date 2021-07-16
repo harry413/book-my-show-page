3ed56cf37a9aaec762220259a36d442ce1195974
harry413 <sachinpatidar413@gmail.com>
2021-07-15 21:02:23 -0700

    git commit -m "add carousal"

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
     <div className="App">
       <h1 className="text-6xl hover:text-blue-500" >Hello sachin!!!</h1>
 
-<button className="border-2 p-1 border-blue-500 rounded bg-blue-500 text-white m-4 hover:bg-red-500">click me</button>
+      <button className="border-2 p-1 border-blue-500 rounded bg-blue-500 text-white m-4 hover:bg-red-500">click me</button>
 
     </div>
   );
---
+++ src/layouts/Default.layout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DefaultLayout = () => {
+    return(
+        <div>
+            <h1 className="text-xl">default layout</h1>
+        </div>
+    );
+};
+
+export default DefaultLayout;
